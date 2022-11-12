1ca56a3595c6499768f9bfcdaaf5192939c8bf2d
master-kim <such0675@naver.com>
2022-11-12 11:54:47 +0900

    2022.11.12.김요한.수정 - 회원가입 시 form 데이터 파일 받기 추가

--- src/main/java/com/meem/stagram/user/UserController.java
+++ src/main/java/com/meem/stagram/user/UserController.java
@@ -3,15 +3,12 @@ package com.meem.stagram.user;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.meem.stagram.dto.RequestDTO;
-import com.meem.stagram.post.IPostService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -98,7 +94,9 @@ public class UserController {
      * */
     @PostMapping("/userRegister")
     public List<HashMap<String, Object>> userRegister(HttpServletRequest request ,
-            @RequestPart("fileInfo") MultipartFile fileInfo) throws Exception{
+            @RequestPart("fileInfo") @Valid @NotNull(message = "파일을 넣어주세요.") MultipartFile fileInfo,
+            @RequestPart("userInfo") @Valid RequestDTO.userRegister userRegister
+            ) throws Exception{
         
         HttpSession session = request.getSession();
         
@@ -106,12 +104,12 @@ public class UserController {
         
         HashMap<String, Object> resultMap = new HashMap<>();
         
-        //try {
-        //    resultMap = iuserservice.userSave(userRegister);
-        //} catch (Exception e) {
-        //    resultMap.put("resultCd", "FAIL");
-        //    resultMap.put("resultMsg", e.getMessage().toString());
-        //}
+        try {
+            resultMap = iuserservice.userSave(fileInfo , userRegister);
+        } catch (Exception e) {
+            resultMap.put("resultCd", "FAIL");
+            resultMap.put("resultMsg", e.getMessage().toString());
+        }
         resultMap.put("resultCd", "SUCC");
         
         resultList.add(resultMap);
